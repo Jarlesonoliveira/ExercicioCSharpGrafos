f57530deed925efacb9742a73f5b7f4f7f2c7102
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-09-26 11:59:57 -0300

    novo projeto 1034

---
+++ Fistival_de_estatuas_de_gelo_1034/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fistival_de_estatuas_de_gelo_1034
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+        }
+    }
+}
