7325f05eecf3199e2bf2ffd73be5a1cefdf48a8f
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-09-30 11:22:40 -0300

    novo projeto 1084

---
+++ Apagando_e_ganhando_1084/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apagando_e_ganhando_1084
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+        }
+    }
+}
