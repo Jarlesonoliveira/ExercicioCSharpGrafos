ea7a25234199b29000aee1af41207d4226da31b9
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-10-06 11:35:47 -0300

    projeto 1912 criado

---
+++ Ajude_seu_madruga_1912/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajude_seu_madruga_1912
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+        }
+    }
+}
