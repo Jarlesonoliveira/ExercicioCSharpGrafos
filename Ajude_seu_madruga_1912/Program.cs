73e35351b0c22ecd29aecf9e4ce781dcb4a276ad
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-10-17 14:59:27 -0300

    projeto 1912 concluído!

--- Ajude_seu_madruga_1912/Program.cs
+++ Ajude_seu_madruga_1912/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Ajude_seu_madruga_1912
 {
     internal class Program
     {
-        static void Main(string[] args) {
+        internal static void Main() {
+            int[] v = new int[100006];
+            int sum;
+            int n;
+            double area;
+            string l;
+            Stopwatch sw = new Stopwatch();
+            
+            while (!(l = Console.ReadLine()).Equals("0 0")) {
+                string[] tempVar = l.Split(' ');
+                n = int.Parse(tempVar[0]);
+                area = int.Parse(tempVar[1]);
+                sum = 0;
+                sw.Start();
+                string[] tempVar1 = new string[n-1];
+                tempVar1 = (Console.ReadLine()).Split(' ');
+                
+                for (int i = 0; i < n; ++i) {
+                    v[i] = int.Parse(tempVar1[i]);
+                    sum += v[i];
+                }
 
+                if (area > sum) {
+                    Console.WriteLine("-.-\n");
+
+                } else if (area == sum) {
+                    Console.WriteLine(":D\n");
+                } else {
+                    double b = 0.0;
+                    double e = 10000.0;
+                    double mid = 0;
+                    
+                    while (b <= e) {
+                        mid = (b + e) / 2.0;
+                        double cut = 0;
+                        for (var i = 0; i < v.Length; i++) {
+                            if (v[i] > mid) {
+                                cut += v[i] - mid;
+                            }
+                        }
+                        double thres = (area - cut);
+                        if (Math.Abs(thres) < 0.00001) {
+                            break;
+                        }
+                        if (cut < area) {
+                            e = mid;
+                        } else {
+                            b = mid;
+                        }
+                    }
+                    Array.Sort(v, 1, n);
+                    Console.WriteLine(mid.ToString("f4", CultureInfo.InvariantCulture));
+                    sw.Stop();
+                    Console.WriteLine("Tempo gasto : " + sw.ElapsedMilliseconds.ToString() + " milisegundos");
+                }                
+            }
         }
     }
 }
--- paradgmas_teste/Program.cs
+++ paradgmas_teste/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +37,83 @@ namespace paradgmas_teste
                 Console.WriteLine(resp);
                 Array.Clear(profitPerDay, 0, profitPerDay.Length);
                 Array.Clear(aux, 0, aux.Length);
+            }
+        }
+    }
+}
+
+
+
+namespace Ajude_seu_madruga_1912
+{
+    internal class Program
+    {
+        public static int[] v = new int[100006];
+        public static int n;
+        public static int area;
+        public static int sum;
+
+        public static double calc(double v2) {
+            double ans = 0.0;
+            for (int i = n - 1; i >= 0; --i) {
+                if (v2 >= v[i]) {
+                    return ans;
+                }
+
+                ans += (double)v[i] - v2;
+            }
+            return ans;
+        }
+
+        public static double bs() {
+            double b = 0.0;
+            double e = (double)v[n - 1];
+            double mid;
+
+            while (b <= e) {
+                mid = (b + e) / 2.0;
+                double c = calc(mid);
+
+                if (Math.Abs(c - (double)area) < 0.0183156) {
+                    return mid;
+                }
+
+                if (c < area) {
+                    e = mid;
+                } else {
+                    b = mid;
+                }
+
+            }
+            return -1;
+        }
+        internal static void Main() {
+            char[] buf = new char[100];
+            string l;
+            while (!(l = Console.ReadLine()).Equals("0 0")) {
+                string[] tempVar = l.Split(' ');
+                n = int.Parse(tempVar[0]);
+                area = int.Parse(tempVar[1]);
+                sum = 0;
+
+                string[] tempVar1 = (Console.ReadLine()).Split(' ');
+                for (int i = 0; i < n; ++i) {
+                    v[i] = int.Parse(tempVar1[i]);
+                    sum += v[i];
+                }
+
+                if (sum < area) {
+                    Console.WriteLine("-.-");
+                } else if (sum == area) {
+                    Console.WriteLine(":D");
+                } else {
+                    Array.Sort(v, 1, n);
+                    buf = string.Format("{0:f4}", bs()).Distinct().ToArray();
+                    Console.WriteLine(buf.Length.ToString("f4", CultureInfo.InvariantCulture));
+
+                }
+
+
             }
         }
     }
