7e2955448a87cabab0bca2bf69fa90fcb944139f
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-10-18 11:42:00 -0300

    projeto 1767 criado

---
+++ soco_do_papai_noel_1767/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soco_do_papai_noel_1767
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+        }
+    }
+}
