87aba86c0f7b3d5ce0e34a574a74b022cb035d4e
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-10-18 16:11:02 -0300

    projeto 1661 criado

---
+++ Comercio_ de_Vinhos_na_Gergovia_1661/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comercio__de_Vinhos_na_Gergovia_1661
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+        }
+    }
+}
--- soco_do_papai_noel_1767/Program.cs
+++ soco_do_papai_noel_1767/Program.cs
@@ -19,7 +19,7 @@ namespace saco_do_papai_noel_1767
             int peso;
             int pac2;
 
-            while(T-- != 0) {
+            while (T-- != 0) {
                 result[0] = 0;
                 pacotes[0] = 0;
                 for (int j = 1; j <= 50; j++) {
@@ -28,17 +28,17 @@ namespace saco_do_papai_noel_1767
                 }
                 pac = int.Parse(Console.ReadLine());
                 pac2 = pac;
-                while(pac-- != 0) {
-                    string[] tempvar = (Console.ReadLine()).Split(' ');
-                    quantidade = int.Parse(tempvar[0]);
-                    peso = int.Parse(tempvar[1]);
+                while (pac-- != 0) {
+                    string[] tempVar1 = (Console.ReadLine()).Split(' ');
+                    quantidade = int.Parse(tempVar1[0]);
+                    peso = int.Parse(tempVar1[1]);
 
-                    for(int i = 50; i >= 0; i--) {
-                        if(result[i] != -1) {
+                    for (int i = 50; i >= 0; i--) {
+                        if (result[i] != -1) {
                             if (i + peso <= 50) {
                                 if (result[i + peso] < result[i] + quantidade) {
                                     result[i + peso] = result[i] + quantidade;
-                                    pacotes[i + peso] = pacotes[i] + 1; 
+                                    pacotes[i + peso] = pacotes[i] + 1;
                                 }
                             }
                         }
@@ -47,17 +47,19 @@ namespace saco_do_papai_noel_1767
 
                 int valor = 0;
                 int pos = 0;
-                for(int i = 0; i < 51; i++) {
+                for (int i = 0; i < 51; i++) {
                     if (result[i] > valor) {
                         valor = result[i];
                         pos = i;
                     }
                 }
+
                 Console.WriteLine(valor + " brinquedos");
-                Console.WriteLine("Peso: " + pos + " kg ");
-                Console.WriteLine("Peso: " +(pac2 + pacotes[pos]) +  " kg ");
+                Console.WriteLine("Peso: " + pos + " kg");
+                Console.WriteLine("sobra(m) " + (pac2 - pacotes[pos]) + " pacote(s)");
                 Console.WriteLine("");
-            }            
+
+            }
         }
     }
 }
