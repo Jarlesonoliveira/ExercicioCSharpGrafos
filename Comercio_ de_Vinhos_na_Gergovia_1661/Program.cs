ee004e3a36bec6f7f46b60f2c6648232768efd70
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-10-19 14:19:42 -0300

    projeto 1661 concluído

--- Comercio_ de_Vinhos_na_Gergovia_1661/Program.cs
+++ Comercio_ de_Vinhos_na_Gergovia_1661/Program.cs
@@ -9,7 +9,29 @@ namespace Comercio__de_Vinhos_na_Gergovia_1661
     internal class Program
     {
         static void Main(string[] args) {
+            string l;
+            int n;
+            long trabalho;
+            while (!(l = Console.ReadLine()).Equals("0")) {
+                string[] tempVar = l.Split(' ');
+                n = int.Parse(tempVar[0]);
+                int[] a = new int[n];
 
+                string[] tempvar1 = (Console.ReadLine()).Split(' ');//o split fora do for ele não repet as vezes
+                for (int i = 0; i < n; i++) {
+                    a[i] = int.Parse(tempvar1[i]);
+                }
+
+                trabalho = 0;
+
+                for (int i = 1; i < n; i++) {
+                    trabalho += Math.Abs(a[i - 1]);
+                    a[i] += a[i - 1];
+                }
+
+                Console.Write(trabalho);
+                Console.Write("\n");
+            }
         }
     }
 }
