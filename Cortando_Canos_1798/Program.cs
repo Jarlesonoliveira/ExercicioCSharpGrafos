121ea7bac7b8612dc326dbabb16d8ca2c083119b
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-10-31 14:49:36 -0300

    projeto 1166 concluído.

--- Cortando_Canos_1798/Program.cs
+++ Cortando_Canos_1798/Program.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Cortando_Canos_1798
 {
     internal class Program
     {
-        static void Main(string[] args) {
+        public static int[,] dp = new int[1005,2005];
+        public static int n;
+        public static int[] valor = new int[1005];
+        public static int[] pesos = new int[1005];
+        public static int calc(int i, int peso) {
+            if (i == n) {
+                return 0;
+            }
+            if (peso == 0) {
+                return 0;
+            }
+            if (peso < 0) {
+                return -9999999;
+            }
+            ref int ans = ref dp[i,peso];
 
+            if (ans == 0) {                
+                ans = Math.Max(calc(i + 1, peso), valor[i] + calc(i, peso - pesos[i]));
+            }
+            return ans;
+        }
+        public static void Main() {            
+            int pmax;
+            string input;
+            List<string> s = new List<string>();
+
+            while ((input = Console.ReadLine()) != null && input != "") {               
+                string[] tempVar = input.Split(' ');
+                n = int.Parse(tempVar[0]);
+                pmax = int.Parse(tempVar[1]); 
+
+                for (int i = 0; i < n; ++i) {
+                    string[] tempVar1 = (Console.ReadLine()).Split(' ');
+                    pesos[i] = int.Parse(tempVar1[0]);
+                    valor[i] = int.Parse(tempVar1[1]);
+                }                
+                Array.Clear(dp, 0, dp.Length);
+                Console.Write(calc(0, pmax));
+                Console.Write('\n');
+
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ Torre_de_Hanoi_ Novamente_1166/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torre_de_Hanoi__Novamente_1166
+{
+    internal class Program
+    {
+        public static int issquare(int x) {
+            return (int)Math.Round(Math.Sqrt(x))
+                * (int)Math.Round(Math.Sqrt(x)) == x ? 1 : 0;
+        }
+        static void Main(string[] args) {
+            int t;
+            t = int.Parse(Console.ReadLine());
+
+            while ((t--) != 0) {
+                int[] pegs = new int[51]; 
+                int n;
+                n = int.Parse(Console.ReadLine());
+                int j = 1;
+                if (n == 1) {
+                    Console.WriteLine(1);
+                    continue;
+                }
+                bool finished = false;
+
+                while (!finished) {
+                    for (int i = 0; i <= n; i++) {
+                        if (issquare(j + pegs[i]) != 0 || pegs[i] == 0) {
+                            pegs[i] = j;
+                            j++;
+                            i = 0;
+                        }
+                        if (i == n) {
+                            Console.WriteLine(j - 1);
+                            finished = true;
+                        }
+                    }
+                }                    
+            }
+            Console.ReadKey();
+        }
+    }
+}
