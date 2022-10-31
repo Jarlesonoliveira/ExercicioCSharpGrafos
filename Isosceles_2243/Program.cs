b15ded184d869aec948f10dd05e177036e1fea9d
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-10-31 14:52:28 -0300

    projeto 2243 criado

---
+++ Isosceles_2243/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isosceles_2243
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+        }
+    }
+}
