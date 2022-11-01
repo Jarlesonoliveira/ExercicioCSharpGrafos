8d0db1aa5023cb74e70e4cb206778bca354e4f07
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-11-01 09:45:26 -0300

    projeto 2446 criado

---
+++ Troco_2446/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troco_2446
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+        }
+    }
+}
