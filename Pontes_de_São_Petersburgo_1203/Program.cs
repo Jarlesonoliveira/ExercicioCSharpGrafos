ea1b02fffded8c57d9f0b805806f5c6efb346a6d
Jarleson Oliveira <jarlesonmello@gmail.com>
2022-11-04 16:03:46 -0300

    projeto 1203 criado

---
+++ Pontes_de_São_Petersburgo_1203/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pontes_de_São_Petersburgo_1203
+{
+    internal class Program
+    {
+
+        static void Main(string[] args) {
+
+        }
+    }
+}
